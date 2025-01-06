34e8da9ba1dd485051db5016dee70929085ca243
apollo-will <willbryan@apollo.com.ph>
2025-01-06 14:44:53 +0800

    feat(download): add download button functionality to prescriptions

--- src/components/MainDashboard/Prescription/Prescription.js
+++ src/components/MainDashboard/Prescription/Prescription.js
@@ -2,6 +2,8 @@ import { useReduxState } from "../../../hooks/useReduxState"
 import NoPrescriptionContent from "./NoPrescriptionContent"
 import style from "./Prescription.module.css"
 import PrescriptionContent from "./PrescriptionContent/PrescriptionContent"
+import { useReactToPrint } from 'react-to-print';
+import React from 'react';
 
 const Prescription = () => {
   const prescription = useReduxState(
@@ -9,9 +11,22 @@ const Prescription = () => {
   )
   const isOpenPrescription = Object.keys(prescription).length > 0
 
+  const componentRef = React.useRef(null);
+
+  const reactToPrintContent = () => {
+    return componentRef.current;
+  };
+
+  const handlePrint = useReactToPrint({
+    documentTitle: "SuperFileName"
+  });
+
   return (
     <div className={style["section-open-prescription"]}>
-      {isOpenPrescription ? <PrescriptionContent /> : <NoPrescriptionContent />}
+      <div className={style["printContent"]} ref={componentRef}>
+        {isOpenPrescription ? <PrescriptionContent /> : <NoPrescriptionContent />}
+      </div>
+      <button onClick={() => handlePrint(reactToPrintContent)}>Download as PDF</button>
     </div>
   )
 }
